ff9cec8094f34b7dc46f4b80f9c7f3ddf77b1e20
citizenz7 <citizenz7@protonmail.com>
2025-04-15 08:24:17 +0200

    fix articles index hight and pagination hight

--- src/Form/RechercheArticleType.php
+++ src/Form/RechercheArticleType.php
@@ -26,7 +26,7 @@ class RechercheArticleType extends AbstractType
         ;
     }
 
-    public function configureOptions(OptionsResolver $resolver)
+    public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => Article::class,
